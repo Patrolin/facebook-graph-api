2ca0c5c265ba9b565a954e4f5d0f54470e1fe65d
Patrolin <patrik@balas.cz>
2023-02-27 14:35:28 +0100

    -ig arg

--- graph_api_auth.py
+++ graph_api_auth.py
@@ -1,3 +1,4 @@
+from sys import argv
 from common import *
 from graph_api import DEFAULT_VERSION, GraphAPIError, GraphApi
 from urllib.parse import urlencode
@@ -46,16 +47,22 @@ class GraphApiAuth:
 
 if __name__ == "__main__":
     # https://developers.facebook.com/apps
-    BUSSINESS_APP = {"id": "1188538135358142", "secret": "58bd3e9660cf9487763cd4a0d89a4a60"}
-    CONSUMER_APP = {"id": "511097751192276", "secret": "35a3ae140472af3bd2d1c8a852570b64"}
+    def test_app(app, perms):
+        auth = GraphApiAuth(app["id"], app["secret"])
+        print(auth.get_auth_url(perms))
+        auth_code = input("Enter auth code: ")
+        access_token = auth.get_access_token_from_code(auth_code)["access_token"]
+        print(access_token)
 
-    app = CONSUMER_APP
-    auth = GraphApiAuth(app["id"], app["secret"])
-    print(auth.get_auth_url("public_profile,email,user_likes,user_birthday,user_gender"))
-    auth_code = input("Enter auth code: ")
-    access_token = auth.get_access_token_from_code(auth_code)["access_token"]
-    print(access_token)
+        graph_api = GraphApi(access_token)
+        me = graph_api.get_object("me")
+        print(me)
 
-    graph_api = GraphApi(access_token)
-    me = graph_api.get_object("me")
-    print(me)
+    if len(argv) < 2 or argv[1] != "-ig":
+        BUSSINESS_APP = {"id": "1188538135358142", "secret": "58bd3e9660cf9487763cd4a0d89a4a60"}
+        CONSUMER_APP = {"id": "511097751192276", "secret": "35a3ae140472af3bd2d1c8a852570b64"}
+        test_app(CONSUMER_APP, "public_profile,email,user_likes,user_birthday,user_gender")
+    else:
+        print("-ig")
+        INSTAGRAM_CONSUMER_APP = {"id": "767685421578450", "secret": "8cb04f923d238b403bbf3265462f1f20"}
+        test_app(INSTAGRAM_CONSUMER_APP, "instagram_basic")
